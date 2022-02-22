42ca2a0c4f340c89191673443548cc820938cc3b
Haeyoung im <younge1009@gmail.com>
2022-02-22 18:19:01 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useRef } from 'react';
+
+const { kakao, cloud } = window;
+let mapRef = null;
+
+function MapContainer() {
+  mapRef = useRef();
+  useEffect(() => {
+    var container = document.getElementById('map');
+    var options = {
+      center: new kakao.maps.LatLng(33.45, 126.57),
+      level: 3
+    };
+    var map = new kakao.maps.Map(container, options);
+    mapRef.curren = map;
+    // 마커삽입
+    var markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+  });
+
+  return (
+    <div id='map' style={{ width: '500px', height: '400px' }} />
+  );
+}
 
 function App() {
+  //useState, useEffect, useRef
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {MapContainer}
+      <button onClick={ ()=>{
+       var pos = new kakao.maps.LatLng(33.5, 126.5);
+       mapRef.current.setCenter(pos);
+      }}>좌표이동</button>
     </div>
   );
 }
